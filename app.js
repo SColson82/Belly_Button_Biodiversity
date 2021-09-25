d87afc98c022d14e93751c4a98169af861772b30
Sharon Colson <sharon.colson@gmail.com>
2021-09-25 14:12:36 -0500

    Finished project

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Initialize the page with a default plot (data for 940)
+// Initialize the page with a default plot.
 function charts(selectedPatientID) {
   d3.json("samples.json").then((data) => {
     var plottingData = data.samples;
@@ -74,7 +74,7 @@ function demo(selectedPatientID) {
       demographicInfoBox.append("h5").text(`${key}: ${value}`);
     });
 
-    // Gauge
+    // Gauge (This is inside the Demographic function because it utilizes the metadata.)
     var guageData = [
       {
         domain: { x: [0, 5], y: [0, 1] },
@@ -83,7 +83,20 @@ function demo(selectedPatientID) {
         type: "indicator",
         mode: "gauge+number",
         delta: { reference: 10 },
-        gauge: { axis: { range: [null, 9] } },
+        gauge: {
+          axis: { range: [null, 9] },
+          steps: [
+            { range: [0, 1], color: "rgb(248, 243, 236)" },
+            { range: [1, 2], color: "rgb(239, 234, 220)" },
+            { range: [2, 3], color: "rgb(230, 225, 205)" },
+            { range: [3, 4], color: "rgb(218, 217, 190)" },
+            { range: [4, 5], color: "rgb(204, 209, 176)" },
+            { range: [5, 6], color: "rgb(189, 202, 164)" },
+            { range: [6, 7], color: "rgb(172, 195, 153)" },
+            { range: [7, 8], color: "rgb(153, 188, 144)" },
+            { range: [8, 9], color: "rgb(132, 181, 137)" },
+          ],
+        },
       },
     ];
 
@@ -97,7 +110,7 @@ function demo(selectedPatientID) {
   });
 }
 
-// Call the data into the inspector console
+// Call the data into the inspector console. 
 function init() {
   d3.json("samples.json").then(function (data) {
     console.log("samples.json:", data);
@@ -107,13 +120,13 @@ function init() {
     data.names.forEach((name) => {
       DropDown.append(`option`).text(name).property(`value`, name);
     });
-
+    // Reset demographic info and visuals to first subject when page is refreshed.
     const firstSample = data.names[0];
     charts(firstSample);
     demo(firstSample);
   });
 }
-
+// Pull data for new subject into demo and visuals. 
 function optionChanged(newSample) {
   charts(newSample);
   demo(newSample);
